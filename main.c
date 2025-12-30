10ff77b0b7c283479eaa1048daee6fe7df7441a1
Tiago Cunha <tiagocunha.me@gmail.com>
2025-12-30 12:06:26 -0300

    feat(main): add player rotation

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
+#include <math.h>
 
-const unsigned int SCREEN_WIDTH = 512;
-const unsigned int SCREEN_HEIGHT = 256;
+#define PI 3.1415926535897932384626
 
-const unsigned int MAP_WIDTH = 16;
-const unsigned int MAP_HEIGHT = 16;
-const unsigned int TILE_SIZE = 32;
+#define MAP_WIDTH 16
+#define MAP_HEIGHT 16
+#define TILE_SIZE 32
 
-const double PLAYER_SPEED = 15.;
+#define SCREEN_WIDTH 512
+#define SCREEN_HEIGHT 256
+
+const double PLAYER_MOV_SPEED = 15.;
+const double PLAYER_ANG_SPEED = .2;
 
 int map[MAP_WIDTH][MAP_HEIGHT];
 
@@ -18,17 +22,12 @@ int walls[][4] = {
     {MAP_WIDTH-1, 0, MAP_WIDTH-1, MAP_HEIGHT-1},
     {MAP_WIDTH-1, MAP_HEIGHT-1, 0, MAP_HEIGHT-1},
     {0, MAP_HEIGHT-1, 0, 0},
-    {1, 1, 3, 1},
-    {1, 2, 9, 2},
-    {8, 5, 14, 5},
-    {7, 7, 9, 9}
+    {6, 6, 9, 9},
+    {3, 12, 12, 12}
 };
 int walls_qty = sizeof(walls) / (sizeof(walls[0][0]) * 4);
 
-double player_x, player_y;
-
-double min(double a, double b) {return a < b ? a : b;}
-double max(double a, double b) {return a > b ? a : b;}
+double player_x, player_y, player_angle, player_mov_x, player_mov_y;
 
 void setWalls() {
     for(int i = 0; i < walls_qty; i++) {
@@ -44,10 +43,10 @@ void drawWalls() {
     glColor3f(1,1,1);
 
     for(int i = 0; i < walls_qty; i++) {
-        int xi = min(walls[i][0], walls[i][2]) * TILE_SIZE;
-        int yi = min(walls[i][1], walls[i][3]) * TILE_SIZE;
-        int xf = (max(walls[i][2], walls[i][0]) + 1) * TILE_SIZE;
-        int yf = (max(walls[i][3], walls[i][1]) + 1) * TILE_SIZE;
+        int xi = fmin(walls[i][0], walls[i][2]) * TILE_SIZE;
+        int yi = fmin(walls[i][1], walls[i][3]) * TILE_SIZE;
+        int xf = (fmax(walls[i][2], walls[i][0]) + 1) * TILE_SIZE;
+        int yf = (fmax(walls[i][3], walls[i][1]) + 1) * TILE_SIZE;
 
         glBegin(GL_QUADS);
         glVertex2i(xi, yi);
@@ -70,12 +69,31 @@ void setPlayerPosition(double x, double y) {
     player_x = x; player_y = y;
 }
 
+void updateAngleMovement(double player_angle) {
+    player_mov_x = cos(player_angle);
+    player_mov_y = sin(player_angle);
+}
+
 void handleKeypress(unsigned char key, int x, int y) {
     switch (key) {
-        case 'a': player_x -= PLAYER_SPEED; break;
-        case 'd': player_x += PLAYER_SPEED; break;
-        case 'w': player_y -= PLAYER_SPEED; break;
-        case 's': player_y += PLAYER_SPEED; break;
+        case 'a':
+            player_angle -= PLAYER_ANG_SPEED;
+            if(player_angle < 0) {player_angle += 2*PI;}
+            updateAngleMovement(player_angle);
+            break;
+        case 'd':
+            player_angle += PLAYER_ANG_SPEED;
+            if(player_angle > 2*PI) {player_angle -= 2*PI;}
+            updateAngleMovement(player_angle);
+            break;
+        case 'w':
+            player_x += player_mov_x * PLAYER_MOV_SPEED;
+            player_y += player_mov_y * PLAYER_MOV_SPEED;
+            break;
+        case 's': 
+            player_x -= player_mov_x * PLAYER_MOV_SPEED;
+            player_y -= player_mov_y * PLAYER_MOV_SPEED;
+            break;
     }
     glutPostRedisplay();
 }
